54e9f0d355f04a5c12e131ca0e0e65d77965a771
Daisho <daishokomiyama@gmail.com>
2016-01-18 00:49:48 -0500

    Initial commit

---
+++ s1v1/index.php
@@ -0,0 +1,35 @@
+<?php
+$pageTitle = "Personal Media Library";
+$section = null;
+
+include("inc/header.php"); ?>
+		<div class="section catalog latest">
+
+			<div class="wrapper">
+
+				<h2>May we suggest something?</h2>
+
+				<ul class="catalog">
+					<li><a href="#">
+					<img src="img/media/design_patterns.jpg" alt="Design Patterns: Elements of Reusable Object-Oriented Software">
+					<p>View Details</p>
+				</a></li>
+				<li><a href="#">
+					<img src="img/media/forest_gump.jpg" alt="Forest Gump">
+					<p>View Details</p>
+				</a></li>
+				<li><a href="#">
+					<img src="img/media/beethoven.jpg" alt="Beethoven: Complete Symphonies">
+					<p>View Details</p>
+				</a></li>
+				<li><a href="#">
+					<img src="img/media/clean_code.jpg" alt="The Clean Coder: A Code of Conduct for Professional Programmers">
+					<p>View Details</p>
+				</a></li>
+				</ul>
+
+			</div>
+
+		</div>
+
+<?php include("inc/footer.php"); ?>
